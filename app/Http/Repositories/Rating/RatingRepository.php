9b50acd8438f5e62c3a3fae0c277767d95d0d337
Swan Yee Myo <swanyeemyo2001@gmail.com>
2024-04-12 15:17:17 +0630

    lastt-commit

--- app/Http/Repositories/Rating/RatingRepository.php
+++ app/Http/Repositories/Rating/RatingRepository.php
@@ -1,37 +1,36 @@
 <?php
+
 namespace App\Http\Repositories\Rating;
 
 use App\Models\Rating;
 
-class RatingRepository implements RatingRepositoryInterface {
-    public function index() {
+class RatingRepository implements RatingRepositoryInterface
+{
+    public function index()
+    {
         return Rating::with('user')->get();
     }
 
-    public function store($request) {
-<<<<<<< HEAD
-       return $data = Rating::create($request->all());
-    } 
-    public function show($id) {
-        return $data = Rating::find($id);
-=======
-       return Rating::create($request);
+    public function store($request)
+    {
+        return Rating::create($request);
     }
 
     public function findById(int $id)
     {
         return Rating::with('user')->where('id', $id)->first();
->>>>>>> 062190778907bd6d80d74d444bf4c2f62494a01f
     }
 
-    public function update($request, $id) {
+    public function update($request, $id)
+    {
 
         $data = Rating::find($id);
         $data->update($request);
         return $data;
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
         Rating::find($id)->delete();
     }
 }
